675ca7d637af261e6f632d7ba81b62e945496ff0
Aschen <amaret93@gmail.com>
2015-01-08 16:48:40 +0100

    refactoring

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,4 +8,28 @@
 
 $CONFIG = array();
 
+// Set environnment dev or prod
+$CONFIG['ENV'] = 'dev';
+
+/**
+ * GENERAL CONFIG
+ */
 $CONFIG['api_url'] = 'webservice';
+if ($CONFIG['ENV'] == 'dev')
+{
+    $CONFIG['hostname'] = 'localhost';
+}
+else if ($CONFIG['ENV'] == 'prod')
+{
+  $CONFIG['hostname'] = 'smart-serveur.aschen.ovh';
+}
+
+/**
+ * MYSQL CONFIG
+ */
+if ($CONFIG['ENV'] == 'dev')
+{
+    $CONFIG['mysql_user'] = 'root';
+    $CONFIG['mysql_password'] = 'wxcvbn';
+    $CONFIG['mysql_database'] = 'smart-serveur';
+}
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 require 'config/config.php';
 require 'flight/Flight.php';
-require 'models/Categorie.php';
+require 'controllers/CategorieController.php';
+require 'models/Model.php';
+
 
 Flight::route('GET /', function(){
-    echo 'hello world!';
+    $BddManager = new Model();
+    $BddManager->ConnectMysql();
+        foreach($BddManager->DoQuery('SELECT * FROM categories') as $row) {
+        print_r($row);
+    }
 });
 
 /**
@@ -13,7 +19,7 @@ Flight::route('GET /', function(){
 Flight::route('GET /categorie/@id', function($id)
 {
     // On appelle la function correspondante
-    Categorie::get($id);   
+    Categorie::get($id);
 });
 
 Flight::route('POST /categorie', function()
@@ -21,12 +27,6 @@ Flight::route('POST /categorie', function()
     // On récupère les paramètres POST
     $nom = filter_input(INPUT_POST, 'nom');
     $image = filter_input(INPUT_POST, 'image');
-
-    // Si il manque un paramètre on renvoi une erreur
-    if ($nom == false || $image == false)
-    {
-        die(Tools::ToJson(array('error' => 'missing parameters')));
-    }
 
     // On appelle la fonction correspondante
     Categorie::post($nom, $image);
